4bbda747e3fe8d7ed84eb6c26a428fa269b486bf
springfieldking <springfieldking@gmail.com>
2023-08-14 16:22:44 +0800

    add p104

---
+++ p104_maximum-depth-of-binary-tree.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include "common.h"
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
